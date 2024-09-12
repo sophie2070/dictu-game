5bb6dca3ae76bfa317969c9869642bb15b1cd293
Rocka2012 <159764466+Rocka2012@users.noreply.github.com>
2024-09-12 15:50:07 +0200

    semi-final commit
    yes

--- dictu/Assets/Scripts/Maikel/ComputerInteract.cs
+++ dictu/Assets/Scripts/Maikel/ComputerInteract.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ComputerInteract : MonoBehaviour
 {
+    private GameObject playeractive;
+    public GameObject speechbubble;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playeractive = other.gameObject;
+            speechbubble.GetComponent<Renderer>().enabled = true;
+        }
     }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playeractive = null;
+            speechbubble.GetComponent<Renderer>().enabled = false;
+        }
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        if (playeractive && Input.GetKeyDown(KeyCode.E)) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }  
 }
